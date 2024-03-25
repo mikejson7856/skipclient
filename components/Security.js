963020dd301034b077ab3c41884747ec7bfe6ff0
Zayn Yusuf <yusue786@gmail.com>
2024-03-25 11:01:46 +0600

    fix design

--- components/Security.js
+++ components/Security.js
@@ -25,7 +25,8 @@ const Security = () => {
         <p className="font-semibold  text-base">
         Copy our verify-team email address:-
         </p>
-        <button className='bg-[#2ba6cb] border-none px-2 py-3 text-white rounded' onClick={copyText}>{userName}Copy Email</button>
+       <span>{userName}</span> 
+       <button className='bg-[#2ba6cb] border-none px-2 py-3 text-white rounded' onClick={copyText}>Copy Email</button>
 
         {/* Modal */}
 
